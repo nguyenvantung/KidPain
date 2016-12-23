509fbb3f1638fc3bc98f68b1432a3ce2f5ecec46
nguyenvantung <nguyentung.fit.hut@gmail.com>
2016-12-23 08:53:43 +0700

    base project

---
+++ app/src/main/java/color/kidpaint/com/kidpaintcolor/fragment/BaseFragment.java
@@ -0,0 +1,49 @@
+package color.kidpaint.com.kidpaintcolor.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Tung Nguyen on 12/20/2016.
+ */
+public abstract class BaseFragment extends Fragment{
+
+    protected View rootView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(getLayoutId(), container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(rootView);
+        initData();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    abstract protected int getLayoutId();
+
+    abstract protected void initView(View root);
+
+    abstract protected void initData();
+}
